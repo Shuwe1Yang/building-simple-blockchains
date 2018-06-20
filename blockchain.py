78744cc5bf7a59e5c4015a0be1c795824474e849
Pau1 <13715125+Shuwe1Yang@users.noreply.github.com>
2018-06-19 22:47:40 -0400

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,253 @@
+import hashlib
+import json
+from time import time
+
+from uuid import uuid4
+from textwrap import dedent
+from flask import Flask, jsonify, request
+
+from urllib.parse import urlparse
+import requests
+
+#Class for blockchain
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        #Creating the genesis block
+        self.new_block(previous_hash=1, proof=100)
+        self.nodes = set()
+    
+    def register_node(self, address):
+        """
+        Adding a new node to the list of nodes
+        :param address: <str> Address of node
+        :return: None
+"""
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+        
+    def valid_chain(self, chain):
+        """
+        Checking if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+"""
+        last_block = chain[0]
+        current_index = 1
+        
+        while current_index < len(chain):
+            block = chain[current_index]
+            print (f'{last_block}')
+            print (f'{block}')
+            print ("\n-----------\n")
+            #Check the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            #Check the proof of work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            
+            last_block = block
+            current_index += 1
+        return True
+    
+    def resolve_conflicts(self):
+        """
+        Using longest chain in the network
+        :return: <bool> True, False if chain is replaced
+"""
+        neighbors = self.nodes
+        new_chain = None
+        #Looking for chains that are longer than ours
+        max_len = len(self.chain)
+        #Grabbing and verifying the chains from all the nodes in our network
+        for node in neighbors:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                #Check if the length is longer and the chain is valid
+                if length > max_len and self.valid_chain(chain):
+                    max_len = length
+                    new_chain = chain
+        #Replacing our chain if we found a longer, valid chain
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
+    
+    #Creating new blocks and adding to the chain
+    def new_block(self, proof, previous_hash=None):
+        """
+        Adding a new block
+        :param proof: <int> The proof given by the Proof of Work Algo
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New Block
+"""
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        #Reset the current list of transactions
+        self.current_transactions = []
+        
+        self.chain.append(block)
+        return block
+        
+        
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Adding a new transaction to the list of transactions
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the block that will hold this transaction
+"""
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index']+1
+    
+    def proof_of_work(self, last_proof):
+        """
+        simple PoW - find a p' to make hash(pp') start with 0000
+                   - p: last proof, p': current proof
+        :param last_proof: <int>
+        :return: <int>
+"""        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+           
+    @property
+    def last_block(self):
+    #Returns the last block in the chain
+        return self.chain[-1]
+    
+    @staticmethod
+    def hash(block):
+    #Hashes a block
+        """
+        SHA-256 hash value
+        :param block: <dict> Block
+        :return: <str>
+"""
+        #Dictionary needs to be ordered
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        is hash(last_proof, proof) starting with 000?
+        :param last_proof: <int>
+        :param proof: <int>
+        :return: <bool> True if correct, False if not
+"""
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+#Blockchain class is done here
+        
+#Implementing Blockchain on Python Flask
+#Instantiate our Node
+app = Flask(__name__)
+#Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('_', '')
+#Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    #Running proof of work algo to get the next proof
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    #Giving rewards to Nodes of POW
+    #Sender is "0" meaning newly mined coin
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_identifier,
+        amount = 1,
+    )
+    
+    #Forge the new block by adding it to the chain
+    block = blockchain.new_block(proof)
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200    
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    #Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    #Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    
+    response = {'message': f'Transaction will be added to Block{index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: plz supply a valid list of nodes", 400
+    for node in nodes:
+        blockchain.register_node(node)
+    
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'Our chain is replaced',
+            'new chain': blockchain.chain,
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
+
+
+
+
+
+    
+    
